bc59ec0b338be87d35ecd1313cfe41b6b61fbafd
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-05-18 11:03:52 -0300

    add: phosphor-icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,26 @@
 import styles from './Header.module.css'
 
+import { MagnifyingGlass } from "@phosphor-icons/react"
+
 export function Header() {
   return (
     <header className={styles.header}>
+
       <div>
         <h1>auto-message</h1>
       </div>
+
       <div>
         <label for="name"></label>
         <input id="name" type="text" placeholder="Buscar" />
       </div>
+
       <div>
-        <button>Buscar</button>
+        <button className={styles.button} title="Buscar">
+          <MagnifyingGlass size={24} />
+        </button>
       </div>
+
     </header >
   )
 }
