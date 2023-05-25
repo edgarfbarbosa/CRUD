f1709dc5892d6bd8bcaa69af95d2de0d5f47f3f4
Edgar Faria Barbosa <edgarfbarbosa@outlook.com>
2023-05-25 17:21:57 -0300

    add: Estrutura básica do Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,40 @@
 import styles from './Modal.module.css'
 
 export function Modal({ isOpen }) {
-  if (isOpen) {
-    return <form></form>
-  } else {
-    return null
-  }
+  if (!isOpen) return null
+
+  return (
+    <div>
+      <form>
+        <h2>Agende sua mensagem</h2>
+
+        <fieldset>
+          <div>
+            <label for="name"></label>
+            <input id="name" type="text" placeholder="Nome" />
+          </div>
+          <div>
+            <label for="surname"></label>
+            <input id="surname" type="text" placeholder="Sobrenome" />
+          </div>
+        </fieldset>
+
+        <div>
+          <label for="number"></label>
+          <input id="number" type="tel" placeholder="Número do seu WhatsApp" />
+        </div>
+
+        <div>
+          <label for="date"></label>
+          <input id="date" type="date" />
+        </div>
+
+        <div>
+          <textarea placeholder="Escreva sua mensagem..." />
+        </div>
+
+        <button>Agendar mensagem</button>
+      </form>
+    </div>
+  )
 }
